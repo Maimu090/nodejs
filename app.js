79e5623927c6ff1828fbc70ad5d41337534b6e7e
Maimuna <maimunaibrahim808@gmail.com>
2024-12-04 21:31:14 +0100

    new commit

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
-const express = require('express');
+const express = require("express");
 
 // express app
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+  console.log("new request made");
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+})
 
- //res.send('<p>home page</p>');
-res.sendFile('./views/index.html', { root: __dirname });
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+})
+app.get("/", (req, res) => {
+   const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
-app.get('/', (req, res) => {
 
-   // res.send('<p>about page</p>');
-   res.sendFile('./views/about.html', { root: __dirname });
-    
-   });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
 
-   // redirects
-   app.get('/about-me', (req, res) => {
-    res.redirect('/about');
-   });
+// redirects
+app.get("/about-me", (req, res) => {
+  res.render("/about", { title: "About" });
+});
 
-   // 404 page
-   app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
-   });
- 
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+});
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});

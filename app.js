b2cd82b9dd0c1f2b8739c9e467d0e7e8d4731be5
Maimuna <maimunaibrahim808@gmail.com>
2024-12-07 17:48:22 +0100

    new changes

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
 const express = require("express");
+const morgan = require("morgan");
+const blog = require('./models/blog');
+const dbURI = 'mongodb+srv://admin:test123@cluster0.0z039.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => app.listen(3000))
 
+
+.catch((err) => console.log(err));
+
+
+
+const mongoose = require('mongoose');
 // express app
 const app = express();
 
 //register view engine
 app.set("view engine", "ejs");
 
-app.listen(3000);
 
-app.use((req, res, next) => {
-  console.log("new request made");
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
+
+app.use(express.static("public"));
+
+
+
+app.use(morgan("dev"));
+
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+  blog.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+  app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
 })
 
 
-app.use((req, res, next) => {
-  console.log("in the next middleware");
-  next();
-})
 app.get("/", (req, res) => {
    const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
@@ -31,6 +61,7 @@ app.get("/", (req, res) => {
 });
 
 
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
@@ -39,6 +70,21 @@ app.get("/about", (req, res) => {
 app.get("/about-me", (req, res) => {
   res.render("/about", { title: "About" });
 });
+
+app.get('/blogs/create', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+  blog.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new Blog" });

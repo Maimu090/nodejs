24ebdcf56199038db0e8a4c03b2f19d9fcbf2269
Maimuna <maimunaibrahim808@gmail.com>
2024-12-09 14:41:56 +0100

    new changes

--- app.js
+++ app.js
@@ -1,95 +1,96 @@
-const express = require("express");
-const morgan = require("morgan");
-const blog = require('./models/blog');
-const dbURI = 'mongodb+srv://admin:test123@cluster0.0z039.mongodb.net/node-tuts?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000))
-
-
-.catch((err) => console.log(err));
-
-
-
+const express = require('express');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 // express app
 const app = express();
 
-//register view engine
-app.set("view engine", "ejs");
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://maimunaibrahim808:maimoon123@cluster0.xy9tq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-
-
-app.use(express.static("public"));
-
-
-
-app.use(morgan("dev"));
-
-app.get('/add-blog', (req, res) => {
-  const blog = new Blog({
-    title: 'new blog',
-    snippet: 'about my new blog',
-    body: 'more about my new blog'
+// Mongoose connection
+mongoose.connect(dbURI, {})
+  .then(() => {
+    console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+    app.listen(3000, () => {
+      console.log('Server is listening on port 3000');
+    });
+  })
+  .catch((error) => {
+    console.error('COULD NOT CONNECT TO DATABASE:', error.message);
   });
-  blog.save()
-  .then((result) => {
-    res.send(result)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
 
-  app.get('/all-blogs', (req, res) => {
-    Blog.find()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  })
-})
+// register view engine
+app.set('view engine', 'ejs');
 
-
-app.get("/", (req, res) => {
-   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render("index", { title: "Home", blogs });
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-
-
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
-// redirects
-app.get("/about-me", (req, res) => {
-  res.render("/about", { title: "About" });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
+// blog routes
 app.get('/blogs/create', (req, res) => {
-  const blog = new Blog({
-    title: 'new blog',
-    snippet: 'about my new blog',
-    body: 'more about my new blog'
-  });
-  blog.save()
-  .then((result) => {
-    res.send(result)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-})
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new Blog" });
+  res.render('create', { title: 'Create a new blog' });
 });
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 // 404 page
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+  res.status(404).render('404', { title: '404' });
 });
